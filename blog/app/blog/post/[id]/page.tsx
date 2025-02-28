f0270ea3dcd52acfc60ed0f63dd620964ee58cf6
karenokasaki <oks.karen@gmail.com>
2025-02-28 06:35:40 -0300

    adding type

--- blog/app/blog/post/[id]/page.tsx
+++ blog/app/blog/post/[id]/page.tsx
@@ -1,11 +1,17 @@
 import Link from "next/link";
 
-export default function Post({ params: { id } }) {
-  return (
-    <div>
-      <h3>Post #{id}</h3>
-      <p>Lorem ipsum</p>
-      <Link href="/blog">Back to blog</Link>
-    </div>
-  );
+interface PostProps {
+   params: {
+      id: string;
+   };
+}
+
+export default function Post({ params: { id } }: PostProps) {
+   return (
+      <div>
+         <h3>Post #{id}</h3>
+         <p>Lorem ipsum</p>
+         <Link href="/blog">Back to blog</Link>
+      </div>
+   );
 }
